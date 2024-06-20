88820c6f54bf3bf4e8d7977b8d945dc01c997e9d
FakhrihanLuthfi <fakhrihanluthfi1@gmail.com>
2024-06-20 20:04:57 +0700

    update Cloud Computing

---
+++ Cloud Computing/src/routes/movie.js
@@ -0,0 +1,19 @@
+/**
+ * @prettier
+ */
+
+"use strict"
+
+const express = require("express")
+
+const movieController = require("../controllers/movie")
+
+const router = express.Router()
+
+router.get("/search", movieController.searchMovies)
+router.get("/popular", movieController.getPopularMovie)
+router.get("/recommendations", movieController.getMovieRecomendations)
+router.get("/prediction", movieController.getMoviePrediction)
+router.get("/:movieId/detail", movieController.getMovieDetail)
+
+module.exports = router
